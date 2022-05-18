ab41def1996682560ead8905262f37b2b628639d
univalchemist <guix7777@outlook.com>
2022-05-19 03:11:38 +0800

    update

--- App.tsx
+++ App.tsx
@@ -8,44 +8,30 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  useColorScheme,
-  useWindowDimensions,
-  StatusBar,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-import {Provider} from 'react-redux';
-import MainNavigator from './src/navigation/MainNavigator';
-import {store} from './src/store/store';
-import {GradientScrollingWrapper} from './src/components/GradientWrpper';
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <Provider store={store}>
-      <SafeAreaView style={{flex: 0, backgroundColor: '#ffe1cc'}} />
-
-      <SafeAreaView style={[styles.Container]}>
-        <MainNavigator />
-      </SafeAreaView>
-    </Provider>
-  );
-};
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-  },
-});
-
-export default App;
+ import React from 'react';
+ import {SafeAreaView, StyleSheet} from 'react-native';
+ 
+ import {Provider} from 'react-redux';
+ import MainNavigator from './src/navigation/MainNavigator';
+ import {store} from './src/store/store';
+ 
+ const App = () => {
+   return (
+     <Provider store={store}>
+       <SafeAreaView style={{flex: 0, backgroundColor: '#ffe1cc'}} />
+ 
+       <SafeAreaView style={[styles.Container]}>
+         <MainNavigator />
+       </SafeAreaView>
+     </Provider>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   Container: {
+     flex: 1,
+   },
+ });
+ 
+ export default App;
+ 
