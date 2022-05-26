6b1f594c5c5d57e8c6d9967c0a89a4a6c85ea9c4
Shiwani <essm.ios2@gmail.com>
2022-05-26 11:46:37 +0530

    app changes

--- App.tsx
+++ App.tsx
@@ -8,30 +8,44 @@
  * @format
  */
 
- import React from 'react';
- import {SafeAreaView, StyleSheet} from 'react-native';
- 
- import {Provider} from 'react-redux';
- import MainNavigator from './src/navigation/MainNavigator';
- import {store} from './src/store/store';
- 
- const App = () => {
-   return (
-     <Provider store={store}>
-       <SafeAreaView style={{flex: 0, backgroundColor: '#ffe1cc'}} />
- 
-       <SafeAreaView style={[styles.Container]}>
-         <MainNavigator />
-       </SafeAreaView>
-     </Provider>
-   );
- };
- 
- const styles = StyleSheet.create({
-   Container: {
-     flex: 1,
-   },
- });
- 
- export default App;
- 
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {COLORS} from './src/theme/color';
+import {BASE_URL} from './src/helpers/constants';
+import {Provider} from 'react-redux';
+import MainNavigator from './src/navigation/MainNavigator';
+import {store} from './src/store/store';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  from,
+} from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: BASE_URL,
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <SafeAreaView style={{flex: 0, backgroundColor: '#ffe1cc'}} />
+        {/*Look into possibly adding gradient view around safe area to remove above spacer */}
+        <SafeAreaView style={[styles.Container]}>
+          <MainNavigator />
+        </SafeAreaView>
+      </Provider>
+    </ApolloProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+  },
+});
+
+export default App;
